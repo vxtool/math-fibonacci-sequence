4ee83c5bc9678d4c1e6c63d73b9f892ff214c227
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-01-20 02:28:42 -0200

    update: examples of python

--- python/generator.py
+++ python/generator.py
@@ -1,8 +1,7 @@
 def fibGen(n,a=0,b=1):
   while n>0:
-      yield a
-      a,b,n = b,a+b,n-1
+    yield a
+    a,b,n = b,a+b,n-1
 
-# >>> [i for i in fibGen(11)]
-
-# [0,1,1,2,3,5,8,13,21,34,55]
+#execute
+print [i for i in fibGen(11)]
--- python/iteration.py
+++ python/iteration.py
@@ -1,5 +1,8 @@
 def fib(n):
   a, b = 0, 1
   for i in range(n):
-      a, b = b, a + b
+    a, b = b, a + b
   return a
+
+#execute
+print fib(10)
